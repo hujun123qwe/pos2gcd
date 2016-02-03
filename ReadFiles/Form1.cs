46a35dc7d4dfd3313fdae6cdc98ccb357580a910
Hu Jun <hujun123qwe@163.com>
2016-02-03 08:32:53 +0800

    some1

--- ReadFiles/Form1.cs
+++ ReadFiles/Form1.cs
@@ -64,6 +64,7 @@ namespace ReadFiles
             int file_number = int.Parse(txt_file_number.Text);
             int file_name_start = int.Parse(txt_file_name_start.Text);
             int end_file_name = file_name_start + file_number;
+            string[] plan_some = new string[3];
             while ((line = pos_reader.ReadLine()) != null)
             {
                 for (int j = 0; j < 4; j++)
@@ -71,16 +72,39 @@ namespace ReadFiles
                     start_line_num = line.IndexOf(',');
                     line = line.Remove(0, start_line_num + 1);
                 }
-                for (int i = 0; i < 6; i++)
+                start_line_num = line.LastIndexOf(',');
+                line = line.Remove(start_line_num, line.Length - start_line_num);
+                
+                for(int i = 0; i < 3; i++)
+                {
+                    start_line_num = line.LastIndexOf(',');
+                    plan_some[i] = line.Substring(start_line_num, line.Length - start_line_num);
+                    line = line.Remove(start_line_num, line.Length - start_line_num);
+                }
+                for(int k = 0; k < 3; k++)
                 {
                     start_line_num = line.LastIndexOf(',');
                     line = line.Remove(start_line_num, line.Length - start_line_num);
                 }
+                line += line + plan_some[0] + plan_some[1] + plan_some[2];
                 if (file_name_start > end_file_name)
                 {
                     MessageBox.Show("文件名大于文件数");
                 }
-                line = file_name + '0' + file_name_start + ".jpg," + line;
+                if (file_name_start < 10)
+                {
+                    line = file_name + "0000" + file_name_start + ".jpg," + line;
+                }
+                else if (file_name_start < 100)
+                {
+                    line = file_name + "000" + file_name_start + ".jpg," + line;
+                }else if (file_name_start < 1000)
+                {
+                    line = file_name + "00" + file_name_start + ".jpg," + line;
+                }else if (file_name_start < 10000)
+                {
+                    line = file_name + "0" + file_name_start + ".jpg," + line;
+                }
                 file_name_start++;
                 txt_gsd.AppendText(line);
                 txt_gsd.AppendText("\r\n");
